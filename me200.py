3e0ad9aea55f42526bf9239e99bc1a6064d8e97f
LenovoT410i <antiohy@mail.ru>
2023-06-04 04:14:33 +0500

    feach: start control version
    starting of control version via the adding first file me200

---
+++ me200.py
@@ -0,0 +1,120 @@
+linux commands: ls: List the contents of a folder
+ls -l : to see details like file size, permission, modified time, etc.
+cd : Change the directory
+cd .. : go upper
+mkdir exemple: create directory
+rmdir exemple: delete directory,
+lsb_release -a :know version of ubuntu
+
+
+GIT:
+    sudo apt install git
+git --version
+sudo apt install git
+
+sudo apt update
+git init: initialisation of git.
+    git config --global user.name "Your Name"
+    git config --global user.email "youremail@domain.com"
+git config --list
+git config --global core.editor "kate": install editor
+git commit: make commit
+nano: To make the commit you should write your commit message, then press ctrl+o to write your message, and then ctrl+x to exit that screen.
+git log: show history of commits
+git branch: show whats branch
+git branch updata_1: creating new branch(updata_1)
+git checkout updata_1: swiching on new branch(updata_1)
+git checkout -b updata_1: create and swiching on new branch(updata_1)
+git branh -D updata_1: delete branch
+git add <>: add files in commit
+git add.: add all in commit
+git log --graph: show branchs(tree)
+git merge <branch>: merging of branchs
+git clone <https://github.com/Anti2000/car_parser.git> <car_parser> : make up clone from github to computor
+git remote -v: show what is connect to computor.
+git remote add origin <https://github.com/Anti2000/example.git> add shotcut
+git push origin master: add file to server  ???
+#################################################################################################
+270 for all.
+120 all ELECTRICITY.
+200 kitchen
+200 other materials
+200 mebel.
+484()200+100
+
+Данные по квартире:
+    Спальня: площадь пола = 10.5, площадь стен = 34.5.
+    Балкон: площадь пола = 4
+    Детская: площадь пола = 14.1, площадь стен = 42,
+    Коридор: площадь пола = 11.0, площадь стен = 47,
+    Кухня: площадь пола = 17.1, площадь стен = 47,
+    общая площадь = 170
+    Ванная: площадь пола = 3.3, площадь стен = 18.4
+
+ВЫВОЗ ВЕЩЕЙ. ленолиума. 17.04( газель 1000 р.) 17.04.2023
+
+ДЕМОНТАЖ ЛИШНИЕГО И ВЫВОЗ.после 17.04.2023
+демонтаж плитки, санузла, ленолиума, обоев, кондиционера 18.04
+(узнать стоимость вывоза мусора)
+(демонтаж, и монтаж кондиционера)
+
+ОТОПЛЕНИЕ.
+добавить батерею, теплый пол на балконе.(узнать технологию укладки теплого пола)
+
+
+КУХНЯ.()(определиться с дизайном, найти кто будет делать, заказать, купить духовой шкаф, микроволновку, газовую плиту) 200000 р.
+Какой фасад -
+Фарнитура -
+ЛДСП -
+40000 р.- Духовой шкаф -
+15000 р. - Микроволновку -
+15000 р - Варочная панель - .
+
+ОТДЕЛКА. (узнать стоимость работа штукатурка, работа шпаклевка)
+штукатурка, шпаклевка 21.04 (закупить материал: штукатурка, шпаклевка, клей, грунтовка)
+работа.
+3000 рублей. - грунтовка -
+48000 р. шпаклевка - небольшой расход смеси – 8,5 кг на 1 м2 (для слоя толщиной 1 см);Штукатурка Кнауф Ротбанд (30 кг) = 2890 кг =
+3000 рублей. грунтовка -
+8000 рублей. шпаклевка -
+
+50000 - электрика, 24.04 (закупить материал)
+
+ПОЛ. (найти кто будет делать, сколько нужно материала)
+стяжка -
+выравнивание полов,(закупить материал, теплый пол на балконе)
+50000 рублей.??? наливной пол -
+35000 кв. ик провода.
+29000 кв. кварцвинил,
+
+ленолиум (закупить кварцивинил, узнать площадь, найти кто будет делать).
+
+санузел.(закупить плитку, ванну, унитаз, зеркало, найти кто будет делать, купить бойлер, ванну, смесители, унитаз, плитка(клей))
+22000 р., плитка
+30000, ванна.
+30000, унитаз.
+5000. зеркало
+8000 . бойлер -
+
+45000 рублей. двери 3 межкомнатные
+50000 рублей. входная дверь
+натяжной потолок(определиться кто будет делать, стоимость)
+
+кухня(установка)
+
+электрика
+
+ОКНО. новое окно.
+
+disassembling:
+29.05.2023- stand up container. 6000
+5500 job.
+
+THE FL0OR:
+will buy details for door. where and how many.
+
+THE WALLS:
+
+THE BATHROOM:
+
+THE ELECTRICITY:
